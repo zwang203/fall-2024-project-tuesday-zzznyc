d8cbe84121b2801d0fb78f5de12b8e86da6ccb33
nwmorgan18 <nwmorgan@usc.edu>
2024-09-17 15:53:54 -0700

    Basic Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] GameObject reticle;
+    [SerializeField] GameObject reticlecenter;
+    //bool spacepressed = false;
+    Rigidbody2D rb;
+    Vector2 playerpos;
+    Vector2 reticlepos;
+    Vector2 direction;
+    [SerializeField] float speed = 10f;
+
+    bool onFloor = false;
+    //[SerializeField] float acceleration = 1f;
+    //[SerializeField] float maxspeed = 300f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("space") && onFloor)
+        {
+            playerpos = (Vector2)transform.position;
+            reticlepos = (Vector2)reticle.transform.position;
+            direction = reticlepos - playerpos;
+            rb.AddForce(direction.normalized * speed);
+        }
+        if (Input.GetKeyUp("r"))
+        {
+            string currentscene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentscene);
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Floor"))
+        {
+            onFloor = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Floor"))
+        {
+            onFloor = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ReticleMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleMove : MonoBehaviour
+{
+    public float rotatespeed = 30f;
+    public GameObject player;
+    //float z = 0f;
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            /*
+            z += Time.deltaTime * rotatespeed;
+            if (z > 360.0f)
+            {
+                z = 0.0f;
+            }
+            transform.localRotation = Quaternion.Euler(0f, 0f, z);
+            */
+            transform.Rotate(new Vector3(0f,0f, -1 * rotatespeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            /*
+            z += Time.deltaTime * rotatespeed;
+            if (z > 360.0f)
+            {
+                z = 0.0f;
+            }
+            transform.localRotation = Quaternion.Euler(0f, 0f, z);
+            */
+            transform.Rotate(new Vector3(0f, 0f, rotatespeed * Time.deltaTime));
+        }
+        transform.position = player.transform.position;
+    }
+}
