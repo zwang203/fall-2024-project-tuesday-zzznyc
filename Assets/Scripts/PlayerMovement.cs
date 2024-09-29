ed8f5e2a49fb6d037b2904166d7b6c892e29fa50
Zhiheng Wang <69889580+zwang203@users.noreply.github.com>
2024-09-29 14:29:25 -0700

    Update PlayerMovement.cs
    set up

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,15 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] GameObject reticle;
     [SerializeField] GameObject reticlecenter;
+    [SerializeField] TextMeshProUGUI stepsText;
     //bool spacepressed = false;
     Rigidbody2D rb;
     Vector2 playerpos;
@@ -15,31 +18,38 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed = 10f;
     [SerializeField] float regGrav = 1.0f;
     [SerializeField] float wallGrav = 0.5f;
-    [SerializeField] int numJumps = 1;
+    [SerializeField] int numJumps = 5;
+
     int currentJumps;
-    bool onWall = false;
     
+
+    bool onWall = false;
+   
+    //[SerializeField] float acceleration = 1f;
+    //[SerializeField] float maxspeed = 300f;
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         currentJumps = numJumps;
+        UpdateStepsText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp("space") && currentJumps > 0)
+        if (Input.GetKeyUp("space") && currentJumps > 0 && onWall) 
         {
             playerpos = (Vector2)transform.position;
             reticlepos = (Vector2)reticle.transform.position;
             direction = reticlepos - playerpos;
-            if (currentJumps < numJumps || onWall)
+            if (currentJumps < numJumps)
             {
                 rb.velocity = Vector3.zero;
             }
             
             rb.AddForce(direction.normalized * speed);
-            currentJumps--;
+            
         }
         if (Input.GetKeyUp("r"))
         {
@@ -50,13 +60,14 @@ public class PlayerMovement : MonoBehaviour
         {
             Application.Quit();
         }
+        UpdateStepsText();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Wall"))
         {
-            currentJumps = numJumps;
+            currentJumps -= 1;
             onWall = true;
             if (rb.velocity.y < 0)
             {
@@ -71,7 +82,7 @@ public class PlayerMovement : MonoBehaviour
         
         if (col.gameObject.CompareTag("Floor"))
         {
-       
+            onWall = true;
             currentJumps = numJumps;
         }
         
@@ -82,11 +93,17 @@ public class PlayerMovement : MonoBehaviour
         if (col.gameObject.CompareTag("Wall"))
         {
             rb.gravityScale = regGrav;
-            onWall = false;
+            onWall = true;
         }
         if (col.gameObject.CompareTag("Floor"))
         {
-            currentJumps = numJumps -1 ;
+            currentJumps = numJumps;
+            onWall = true;
         }
+        onWall = false;
+    }
+    private void UpdateStepsText()
+    {
+        stepsText.text = "Steps Remaining: " + currentJumps.ToString();
     }
 }
